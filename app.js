d87bc024193ee095cc6fbd72fc8b83a3a783169b
AlyonaTiki <31675522+AlyonaTiki@users.noreply.github.com>
2018-04-14 11:52:04 -0400

    selection from dropdown menu

--- app.js
+++ app.js
@@ -1,36 +1,27 @@
-$(document).ready(function() {
-	$('#query').on("keypress", function(event) {
-		if (event.which == 13) {
-			var offset = Math.round(Math.random() * 1000);
-			var query = this.value;
-			var key = "P7NLCftNCTpBvJr3RlwWp8YUZ9RTcKgy";
-			var url = "http://api.giphy.com/v1/gifs/search?q="
-						+ query
-						+ "&api_key="
-						+ key
-						+ "&limit=20"
-						+ "&offset="
-						+ offset;
-			
-			$.getJSON(url, function(json) {
-				console.log(json);
-                $('')
-				document.getElementById('gifs').innerHTML = "";
-				for (let i = 0; i < json.data.length; i++) {
-					const img = json.data[i];
-					if (img.images.downsized.url) {
-						const imgElem = $('<img>')
-							.attr('src', img.images.downsized.url);
-					
-						const imgContainer = $('<div>')
-							.addClass('gif');
-					
-						imgContainer.append(imgElem);
-					
-						 $('#gifs').append(imgContainer);
-					}
-				}
-			});
-		}
-	});
+$(document).ready(function () {
+
+    $("select").change(function () {
+            var str = "";
+            $("select option:selected").each(function () {
+                str += $(this).text();
+            });
+            var query = str;
+            var key = "kAv1sgh2zjMEG6nofwfLcu5pA55n4LEO";
+            var url = "http://api.giphy.com/v1/gifs/search?q=" +
+                query +
+                "&api_key=" +
+                key +
+                "&limit=1";
+//            console.log(url);
+            $.getJSON(url, function (json) {
+                for (let i = 0; i < json.data.length; i++) {
+                    const img = json.data[i];
+                    const imgElem = $('<img>')
+                        .attr('src', img.images.downsized.url);
+                    $('#gifs').append(imgElem);
+//                    console.log(img);
+                }
+            });
+        })
+        .trigger("change");
 });
