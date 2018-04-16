ca19f21238d10d120c23e7da7935ce4857fd98ea
cwong984 <cwong984@gmail.com>
2018-04-16 19:13:15 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ $(document).ready(function () {
     $("select").change(function () {
             var str = "";
             $("select option:selected").each(function () {
-                str += $(this).text();
+                str += "+" + $(this).text();
             });
-            var query = str;
+            //var query = this.value;
+            str = str.substr(1, str.length);    
+            console.log(str);
+        var query = str;
             var key = "kAv1sgh2zjMEG6nofwfLcu5pA55n4LEO";
-            var url = "http://api.giphy.com/v1/gifs/search?q=" +
+            var url = "http://api.giphy.com/v1/gifs/search?q=offset:integer(int5)" +
                 query +
                 "&api_key=" +
                 key +
                 "&limit=1";
-//            console.log(url);
+           console.log(url);
             $.getJSON(url, function (json) {
+                console.log(json)
+                
                 for (let i = 0; i < json.data.length; i++) {
                     const img = json.data[i];
                     const imgElem = $('<img>')
